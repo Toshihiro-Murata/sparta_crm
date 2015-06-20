8d5984a74cda6a8731b7a65da367343786731edb
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-20 18:31:07 +0900

    Add Ransack search to customer#index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,9 @@ class CustomersController < ApplicationController
   before_action :set_customer, only: [:edit, :update, :show, :destroy ]
 
   def index
-    @customers = Customer.page(params[:page])
+    #@customers = Customer.page(params[:page])
+    @q = Customer.ransack(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
