b8768ba2df5b07dcbc5070cae97ef0d02cde9977
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-21 10:48:32 +0900

    Remove only option from before_action :authenticate_user! .

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:edit, :update, :show, :destroy ]
-  before_action :authenticate_user!, only: [ :edit, :create ]
+  before_action :authenticate_user!
 
   def index
     #@customers = Customer.page(params[:page])
